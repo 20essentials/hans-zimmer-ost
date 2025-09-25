4df34dd2d508cf0ce0fa84a0b8952e51a0c49832
DominicDev <s0l0estudi4r@gmail.com>
2025-09-25 20:05:25 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HANS ZIMMER';
+export const nameSongs = [
+	"THE DARK KNIGHT THEME", /*1*/
+	"MOMBASA ", /*2*/
+	"HE'S A PIRATE", /*3*/
+	"TIME ", /*4*/
+	"INTERSTELLAR THEME", /*5*/
+	"DRINK UP ME HEARTIES YO HO", /*6*/
+	"THE LION KING THEME", /*7*/
+	"CHEVALIERS DE SANGREAL", /*8*/
+	"EARTH ", /*9*/
+	"HONOR HIM ", /*10*/
+	"NOW WE ARE FREE", /*11*/
+	"HOIST THE COLOURS", /*12*/
+	"CORNFIELD CHASE", /*13*/
+	"BARBARIAN HORDE", /*14*/
+	"DREAM IS COLLAPSING", /*15*/
+	"A WAY OF LIFE", /*16*/
+	"NO TIME FOR CAUTION", /*17*/
+	"YOU'RE SO COOL", /*18*/
+	"THE BATTLE", /*19*/
+	"ELYSIUM ", /*20*/
+]
